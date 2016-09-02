a110a979de9ddf36d69d4a6231b1567efd684cf6
Mudassir <mudassir@azureiken.com>
2016-09-02 16:25:59 +0530

    Updated spree host url

--- config/constant.js
+++ config/constant.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   spree: {
-    host: 'http://104.215.254.150:3000', //Spree store-Host Url
+    host: 'https://test.store.luxire.com', //Spree store-Host Url
     products: '/api/products',
     customerProducts: '/customized_taxons/get_taxon_details.json' 
 
