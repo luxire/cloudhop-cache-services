cd7e80abb4120ac732ee39f451c162830ff66e0b
Mudassir <mudassir@azureiken.com>
2017-01-19 14:54:28 +0530

    Spree url change

--- config/constant.js
+++ config/constant.js
@@ -2,8 +2,7 @@
 
 module.exports = {
   spree: {
-    host: 'http://luxire-store.stage.cloudhop.in', //Spree store-Host Url for dev
-    // host: 'https://luxire-store.cloudhop.in', //Spree store-Host Url for prod
+    host: 'https://test.store.luxire.com', //Spree store-Host Url for prod
     products: '/api/products',
     customerProducts: '/customized_taxons/get_taxon_details.json' 
 
