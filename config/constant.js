97f7d31f1a5802c03f3e79940fac14c16cd8388b
Mudassir <mudassir@azureiken.com>
2017-01-31 02:09:51 +0530

    Change spree url based on env

--- config/constant.js
+++ config/constant.js
@@ -1,8 +1,10 @@
 'use strict';
 
+var alpha_url = "https://test.store.luxire.com";
+var beta_url = "https://luxire-store.cloudhop.in/";
 module.exports = {
   spree: {
-    host: 'https://test.store.luxire.com', //Spree store-Host Url for prod
+    host: process.env.NODE_ENV === 'production' ? beta_url : alpha_url, //Spree store-Host Url for prod
     products: '/api/products',
     customerProducts: '/customized_taxons/get_taxon_details.json' 
 
