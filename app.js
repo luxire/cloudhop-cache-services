37cccef398048525fab4e3e7b4cfddc5c35c792c
Mudassir <mudassir@azureiken.com>
2017-05-18 12:14:13 +0530

    Increased body parser limit to 5Mb

--- app.js
+++ app.js
@@ -22,7 +22,10 @@ app.use(compression());
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.json());
+
+// Increased the limit of JSON parse to 5mb. Done by Manish
+app.use(bodyParser.json({limit: '5mb'}));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
