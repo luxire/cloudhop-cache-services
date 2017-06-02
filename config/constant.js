7e25e52137e7c63c3a892827a5fc1c10aa105eb5
Mudassir <mudassir@azureiken.com>
2017-06-02 19:09:04 +0530

    Added new object redis conf

--- config/constant.js
+++ config/constant.js
@@ -2,11 +2,14 @@
 
 var alpha_url = "https://test.store.luxire.com";
 var beta_url = "https://luxire-store.cloudhop.in/";
+console.log('host is ',  process.env.NODE_REDIS_HOST);
 module.exports = {
   spree: {
     host: process.env.NODE_ENV === 'production' ? beta_url : alpha_url, //Spree store-Host Url for prod
     products: '/api/products',
-    customerProducts: '/customized_taxons/get_taxon_details.json' 
-
+    customerProducts: '/customized_taxons/get_taxon_details.json'
+  },
+  redisConf: {
+    host: process.env.NODE_REDIS_HOST // The redis's server ip
   }
 }
